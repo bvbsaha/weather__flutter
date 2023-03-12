3a3275109475d6a4df991efde2513f3a67a25849
bvbsaha <94446411+bvbsaha@users.noreply.github.com>
2023-03-12 15:52:55 +0300

    Initial commit

---
+++ lib/widget/current_weather.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//
+import '../utils/constanst.dart';
+
+Widget currentWeather({
+  required VoidCallback onPressed,
+  required String? temp,
+  required String? location,
+  required String? country,
+  required String? status,
+}) {
+  return Container(
+    width: w,
+
+    child: Container(
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              Row(
+                children: [
+                  Text("$location, $country", style: kTitleFont),
+                  SizedBox(width: 10,),
+                  IconButton(
+                      onPressed: onPressed,
+                      icon: Icon(
+                        CupertinoIcons.refresh,
+                        color: Colors.white,
+                        size: 30,
+                      )),
+                ],
+              ),
+              Text("${temp}°", style: kTempFont),
+            ],
+          ),
+          Expanded(child: Container()),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            width: w / 13,
+            height: h / 5,
+            child: RotatedBox(
+              quarterTurns: -1,
+              child: Center(
+                child: Text(
+                  status!,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
